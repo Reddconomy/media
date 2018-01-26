3d55b7d06538765eb1db14b048df2717406bbe27
Riccardo Balbo <riccardo@forkforge.net>
2018-01-26 14:17:01 +0100

    Add webm videos

---
+++ media/webm/index.html.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo "<html>
+<head>
+<style>
+body{
+	text-align:center;
+	background:#000;
+}
+video {
+ width: 560px;
+ height: 415px;
+ margin:30px;
+ 
+}
+</style>
+</head>
+<body>
+" > index.html
+
+for f in *.webm;
+do
+	echo "<video controls autoplay preload='none' loop>
+		  <source src='$f' type='video/webm' >
+	      </video>
+	" >> index.html
+
+done
+
+echo "
+</body>
+</html>
+" >> index.html
